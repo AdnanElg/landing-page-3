98873318dacee6ff26d84ef75e5ccb9bb0bd02a1
El Ghalbzouri Adnan <elghalbzouriadnan@OrdinatabledeEl.lan>
2024-08-19 11:45:41 +0200

    fix

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,20 +1,30 @@
-// ******* START NAVBAR ********** //
-
 const toggleButton = document.getElementById("btn-menu");
 const hamburgerIcon = toggleButton.querySelector("img:nth-child(1)");
 const closeIcon = toggleButton.querySelector("img:nth-child(2)");
 const menuLists = document.getElementById("lists-menu");
 
-toggleButton.addEventListener("click", () => {
-  if (hamburgerIcon.style.display === "none") {
-    hamburgerIcon.style.display = "block";
-    closeIcon.style.display = "none";
-    menuLists.style.display = "none";
-  } else {
+function toggleMenuVisibility(isVisible) {
+  hamburgerIcon.style.display = isVisible ? "none" : "block";
+  closeIcon.style.display = isVisible ? "block" : "none";
+  menuLists.style.display = isVisible ? "flex" : "none";
+}
+
+function updateMenuVisibility() {
+  const isDesktop = window.innerWidth >= 1024;
+  if (isDesktop) {
     hamburgerIcon.style.display = "none";
-    closeIcon.style.display = "block";
+    closeIcon.style.display = "none";
     menuLists.style.display = "flex";
+  } else {
+    const isMenuVisible = closeIcon.style.display === "block";
+    toggleMenuVisibility(isMenuVisible);
   }
+}
+
+toggleButton.addEventListener("click", () => {
+  const isMenuVisible = hamburgerIcon.style.display === "none";
+  toggleMenuVisibility(!isMenuVisible);
 });
 
-// ******* END NAVBAR ********** //
+window.addEventListener("load", updateMenuVisibility);
+window.addEventListener("resize", updateMenuVisibility);
